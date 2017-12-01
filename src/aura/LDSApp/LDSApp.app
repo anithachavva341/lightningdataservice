cfd70eea0883fb5bfd7e75b3a4d5b3a952d3f43b
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2017-12-01 14:12:15 +0530

    Added LDSCompEdit for update operation in LDS and modified LDSApp

--- src/aura/LDSApp/LDSApp.app
+++ src/aura/LDSApp/LDSApp.app
@@ -1,3 +1,4 @@
 <aura:application extends="force:slds">
-    <c:LDSComp recordId="0017F000009csczQAA" />
+    <c:LDSComp recordId="00Q7F000002HGG6UAO" />
+    <c:LDSCompEdit recordId="00Q7F000002HGG6UAO" />
 </aura:application>
---
+++ src/aura/LDSCompEdit/LDSCompEditHelper.js
@@ -0,0 +1,20 @@
+({
+	saveRecord : function(component) {
+        component.find("recordEdit").saveRecord($A.getCallback(function(saveResult) {
+            if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+	            var resultsToast = $A.get("e.force:showToast");
+	            resultsToast.setParams({
+	                "title": "Saved",
+	                "message": "The record saved Successfully"
+	            });
+	            resultsToast.fire();            	
+            } else if (saveResult.state === "INCOMPLETE") {
+                console.log("User is offline, device doesn't support drafts.");
+            } else if (saveResult.state === "ERROR") {
+                console.log('Problem saving record, error: ' + JSON.stringify(saveResult.error));
+            } else {
+                console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+            }
+        }));
+	}
+})
